648706cb2a26cfaf3b09cf967af3a62955e33fd2
Subeen9 <subin.bista@selu.edu>
2023-10-17 18:10:27 -0500

    Student page updated

--- src/pages/Grades/Grades.js
+++ src/pages/Grades/Grades.js
@@ -3,51 +3,54 @@ import { Table } from "react-bootstrap";
 
 const Grades = ({ matchingId, grades }) => {
   return (
-    <div>
-      <div className="mt-4">
-        <table style={{ border: "1px solid black" }}>
-          <thead style={{ border: "1px solid black" }}>
-            <tr
-              className="table-tr"
-              style={{
-                fontSize: "16px",
-                color: "#777",
-                border: "1px solid black",
-              }}
-            >
-              <th>{grades[0].t2item110}</th>
-              <th>{grades[0].t2item130}</th>
-              <th>{grades[0].t2item140}</th>
-              <th>{grades[0].t2item150}</th>
-              <th>{grades[0].t2item160}</th>
-              <th>{grades[0].t2item170}</th>
-              <th>{grades[0].t2item180}</th>
-              <th>{grades[0].t2item190}</th>
-            </tr>
-          </thead>
-          {grades.slice(2).map((grade) => {
-            return (
-              <tbody className="table-tbody">
-                {grade.t2key120 === matchingId.t1key110 ? (
-                  <tr style={{ fontSize: "15px", border: "1px solid black" }}>
-                    <td>{grade.t2item110}</td>
-                    <td>{grade.t2item130}</td>
-                    <td>{grade.t2item140}</td>
-                    <td>{grade.t2item150}</td>
-                    <td>{grade.t2item160}</td>
-                    <td>{grade.t2item170}</td>
-                    <td>{grade.t2item180}</td>
-                    <td> {grade.t2item190}</td>
-                  </tr>
-                ) : (
-                  <></>
-                )}
-              </tbody>
-            );
-          })}
-        </table>
-      </div>
-    </div>
+
+    <>
+    </>
+    // <div>
+    //   <div className="mt-4">
+    //     <table style={{ border: "1px solid black" }}>
+    //       <thead style={{ border: "1px solid black" }}>
+    //         <tr
+    //           className="table-tr"
+    //           style={{
+    //             fontSize: "16px",
+    //             color: "#777",
+    //             border: "1px solid black",
+    //           }}
+    //         >
+    //           <th>{grades[0].t2item110}</th>
+    //           <th>{grades[0].t2item130}</th>
+    //           <th>{grades[0].t2item140}</th>
+    //           <th>{grades[0].t2item150}</th>
+    //           <th>{grades[0].t2item160}</th>
+    //           <th>{grades[0].t2item170}</th>
+    //           <th>{grades[0].t2item180}</th>
+    //           <th>{grades[0].t2item190}</th>
+    //         </tr>
+    //       </thead>
+    //       {grades.slice(2).map((grade) => {
+    //         return (
+    //           <tbody className="table-tbody">
+    //             {grade.t2key120 === matchingId.t1key110 ? (
+    //               <tr style={{ fontSize: "15px", border: "1px solid black" }}>
+    //                 <td>{grade.t2item110}</td>
+    //                 <td>{grade.t2item130}</td>
+    //                 <td>{grade.t2item140}</td>
+    //                 <td>{grade.t2item150}</td>
+    //                 <td>{grade.t2item160}</td>
+    //                 <td>{grade.t2item170}</td>
+    //                 <td>{grade.t2item180}</td>
+    //                 <td> {grade.t2item190}</td>
+    //               </tr>
+    //             ) : (
+    //               <></>
+    //             )}
+    //           </tbody>
+    //         );
+    //       })}
+    //     </table>
+    //   </div>
+    // </div>
   );
 };
 
--- src/pages/Students/Students.js
+++ src/pages/Students/Students.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
-import imageLogo from "../../images/logo.png";
-import { database, db } from "../../firebase";
-import { ref, set } from "firebase/database";
+import { Form } from "react-bootstrap";
+import './Students.css';
+import {FaUserTie} from "react-icons/fa";
+import { FaPencilAlt } from 'react-icons/fa';
+import {FaUniversity} from 'react-icons/fa';
+import {FaPhone} from 'react-icons/fa';
+import {FaEnvelopeOpen} from 'react-icons/fa';
+import {FaRegCalendarTimes} from 'react-icons/fa';
 
 const Students = ({ matchingId, data, isEdit, saveData }) => {
   const [t1key110, sett1key110] = useState(matchingId.t1key110);
@@ -10,172 +15,124 @@ const Students = ({ matchingId, data, isEdit, saveData }) => {
   const [t1item140, setPhone] = useState(matchingId.t1item140);
   const [t1item150, setEmail] = useState(matchingId.t1item150);
   const [t1item160, setRegistrationDate] = useState(matchingId.t1item160);
-  const editMode = isEdit ? "pointer-cursor" : "";
+  let isEditing = false;
+  
 
-  if (saveData) {
-    set(ref(database, `vbts/updatedData/${t1key110}`), {
-      t1key110: t1key110,
-      t1item120: t1item120,
-      t1item130: t1item130,
-      t1item140: t1item140,
-      t1item150: t1item150,
-    })
-      .catch((e) => {
-        console.log("SAVING ERROR", e);
-      })
-      .then(() => console.log("DATA POSTED SUCCESSFULLY"));
-    console.log("DATA SAVED");
-  }
+
+  console.log("🚀 ~ file: Students.js:7 ~ Students ~ t1key110:", t1key110)
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (isEditing) {
+      saveData();
+    }
+  };
+  try{
+     var time = t1item160.split('-');
+     console.log(time);
+     var time1 = time[0] + time[1];
+     var time2 = time[2]. split(':');
+     var newTime = time[0] + "-" + time[1] + "-" + time2[0] + ":" + time2[1];
+     console.log(newTime);
+
+}
+ catch(error){
+  console.log(error)
+ }
 
   return (
-    <div>
-      <div className="d-flex justify-content-between">
-        {/* this is the logo part */}
-        <div style={{ width: "200px", height: "200px" }}>
-          <img
-            style={{ width: "100%", height: "100%" }}
-            src={imageLogo}
-            alt="logo"
-          />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-        >
-          <p style={{ fontSize: "35px" }}>Transcript</p>
-        </div>
-        {/* this is the left part */}
-        <div className="">
-          <div>
-            <div>
-              {data[1].t1key110 !== "null" && (
-                <p>
-                  <strong>{data[0].t1key110}:&nbsp;</strong>
-                  {isEdit ? (
-                    <input
-                      type="text"
-                      value={t1key110}
-                      className="pointer-cursor"
-                      onChange={(e) => {
-                        sett1key110(e.target.value);
-                      }}
-                    />
-                  ) : (
-                    <span>{t1key110}</span>
-                  )}
-                </p>
-              )}
-            </div>
-            <div>
-              {data[1].t1item120 !== "null" && (
-                <p>
-                  <strong>{data[0].t1item120}:&nbsp;</strong>
-                  <span
-                    contentEditable={isEdit}
-                    className={editMode}
-                    onChange={(e) => {
-                      setName(e.target.value);
-                    }}
-                  >
-                    {t1item120}
-                  </span>
-                </p>
-              )}
-            </div>
-            <div>
-              {data[1].t1item130 !== "null" && (
-                <p>
-                  <strong>{data[0].t1item130}:&nbsp;</strong>
-                  <span
-                    contentEditable={isEdit}
-                    className={editMode}
-                    onChange={(e) => {
-                      setProgram(e.target.value);
-                    }}
-                  >
-                    {t1item130}
-                  </span>
-                </p>
-              )}
-            </div>
-            <div>
-              {data[1].t1item140 !== "null" && (
-                <p>
-                  <strong>{data[0].t1item140}:&nbsp;</strong>
-                  <span
-                    contentEditable={isEdit}
-                    className={editMode}
-                    onChange={(e) => {
-                      setPhone(e.target.value);
-                    }}
-                  >
-                    {t1item140}
-                  </span>
-                </p>
-              )}
-            </div>
-            <div>
-              {data[1].t1item150 !== "null" && (
-                <p>
-                  <strong>{data[0].t1item150}:&nbsp;</strong>
-                  <span
-                    contentEditable={isEdit}
-                    className={editMode}
-                    onChange={(e) => {
-                      setEmail(e.target.value);
-                    }}
-                  >
-                    {t1item150}
-                  </span>
-                </p>
-              )}
-            </div>
-            <div>
-              {data[1].t1item160 !== "null" && (
-                <p>
-                  <strong>{data[0].t1item160}:&nbsp;</strong>
-                  <span
-                    className={editMode}
-                    onChange={(e) => {
-                      setRegistrationDate(e.target.value);
-                    }}
-                  >
-                    {t1item160}
-                  </span>
-                </p>
-              )}
-            </div>
-            {/* <div>
-            {data[1].t1item112 != "null" && 
-              <p>
-                <strong>{data[0].t1item112}:&nbsp;</strong>
-                <span>{matchingId.t1item112}</span>
-              </p>
-            }
-            </div>
-            <div>
-            {data[1].t1item190 != "null" && 
-              <p>
-                <strong>{data[0].t1item190}:&nbsp;</strong>
-                <span>{matchingId.t1item190}</span>
-              </p>
-            }
-            </div>
-            <div>
-            {data[1].t1item200 != "null" && 
-              <p>
-                <strong>{data[0].t1item200}:&nbsp;</strong>
-                <span>{matchingId.t1item200}</span>
-              </p>
-            }
-            </div> */}
-          </div>
-        </div>
-      </div>
+    <>
+    <div className="form-heading"> 
+     <h2>Student Registration </h2>
     </div>
+    <div className= "Student-form" >
+      
+    
+    <div className="form-box">
+      <Form  onSubmit={handleSubmit} className="main">
+     < FaUserTie/>
+        <Form.Label className="form-label">{data[0].t1key110}</Form.Label>
+        <Form.Control className="form-control"
+        type = "text"
+        value={t1key110}
+        readOnly 
+        onChange={(event)=>{
+          sett1key110(event.target.value)
+        }}
+        
+        />
+      </Form>
+      <Form  onSubmit={handleSubmit} className="main">
+        <FaPencilAlt/>
+        <Form.Label className="form-label">{data[0].t1item120}</Form.Label>
+        <Form.Control className={isEdit ? "editable" :"form-control"}
+        type = "text"
+        value={t1item120}
+        onChange={(event)=>{
+          setName(event.target.value)
+        }}
+        readOnly ={!isEdit}
+        />
+      </Form>
+      <Form  onSubmit={handleSubmit} className="main">
+      <FaUniversity/>
+        <Form.Label className="form-label">{data[0].t1item130}</Form.Label>
+        <Form.Control className={isEdit ? "editable" :"form-control"}
+        type = "text"
+        value={t1item130}
+        onChange={(event)=>{
+          setProgram(event.target.value)
+        }}
+        readOnly ={!isEdit}
+        />
+      </Form>
+      <Form  onSubmit={handleSubmit} className="main" >
+      <FaPhone/>
+        <Form.Label className="form-label">{data[0].t1item140}</Form.Label>
+        <Form.Control className={isEdit ? "editable" :"form-control"}
+        type = "text"
+        value={t1item140}
+        
+        onChange={(event)=>{
+          setPhone(event.target.value)
+        }}
+        readOnly ={!isEdit}
+        />
+      
+      </Form>
+      <Form  onSubmit={handleSubmit} className="main">
+      <FaEnvelopeOpen/>
+
+        <Form.Label className="form-label">{data[0].t1item150}</Form.Label>
+        <Form.Control className={isEdit ? "editable" :"form-control"}
+        type = "text"
+        value={t1item150}
+        onChange={(event)=>{
+          setEmail(event.target.value)
+        }}
+        readOnly ={!isEdit}
+        />
+      </Form>
+      <Form  onSubmit={handleSubmit} className="main " >
+      <FaRegCalendarTimes/>
+        <Form.Label className="form-label">{data[0].t1item160}</Form.Label>
+        <Form.Control className="form-control"
+        type = "text"
+        readOnly
+        value={newTime}
+        onChange={(event)=>{
+          setRegistrationDate(event.target.value)
+        }}
+      
+        />
+      </Form>
+      </div>
+     
+ 
+</div>
+
+      
+    </>
   );
 };
 
